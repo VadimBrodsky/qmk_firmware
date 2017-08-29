81feabf5f51955f85be39f83853e65c451b37a1b
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-08-29 08:30:22 -0400

    Revert "Place enter where right space is, add right shift"
    This reverts commit 519c8176eb859f94c31a4f2c559a52d962754649.

--- keyboards/lets_split/keymaps/vdk9000/keymap.c
+++ keyboards/lets_split/keymaps/vdk9000/keymap.c
@@ -32,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Hyper| Alt  |Lower |Space |Enter |Raise |   -  |   =  |   [  |  ]   |
+ * | Ctrl | GUI  | Hyper| Alt  |Lower |Space |Space |Raise |   -  |   =  |   [  |  ]   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, KC_LGUI, KC_HYPR, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LCTL, KC_LGUI, KC_HYPR, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC  \
 ),
 
 /* Lower
