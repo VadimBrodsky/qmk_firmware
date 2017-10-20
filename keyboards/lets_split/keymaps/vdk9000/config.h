77ce590403f1a70fe5f5d790a0aea4fa39754a22
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-10-20 10:45:29 -0400

    Remove mouse mode

--- keyboards/lets_split/keymaps/vdk9000/config.h
+++ keyboards/lets_split/keymaps/vdk9000/config.h
@@ -21,12 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 #define LEADER_TIMEOUT 500
-#define MOUSEKEY_DELAY             300
-#define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         30
-#define MOUSEKEY_TIME_TO_MAX       20
-#define MOUSEKEY_WHEEL_MAX_SPEED   10
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 30
 
 #include "../../config.h"
 
