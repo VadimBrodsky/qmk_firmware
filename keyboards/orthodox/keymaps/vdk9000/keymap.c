ac304e42f1c5950348525e28277bf2157b6bb556
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-06-04 13:05:16 -0400

    Fix build errors

--- keyboards/orthodox/keymaps/vdk9000/keymap.c
+++ keyboards/orthodox/keymaps/vdk9000/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP( \
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_BSPC, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_LCTL, KC_LGUI, _______, KC_LEAD,          KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, \
-    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_LALT, KC_SPC,  KC_SPC,  KC_ENT, RAISE,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_RSPC  \
+    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_LALT, KC_SPC,  KC_SPC,  KC_ENT,   RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_RSPC  \
   ),
 
 /* Lower
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------------------------------------------'
  */
   [_LOWER] = KEYMAP( \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                       _______, _______, KC_INSERT, _______, _______, KC_BSPC, \
-    _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_EQL,           _______, _______, _______, _______,          KC_HOME, KC_PGDOWN, KC_PGUP, KC_END,  _______, KC_PIPE, \
-    KC_LCBR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, KC_RCBR  \
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                       _______, _______,   KC_INSERT, _______, _______, KC_DEL,  \
+    _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_EQL,           _______, _______, _______, _______,          KC_HOME, KC_PGDOWN, KC_PGUP,   KC_END,  _______, KC_PIPE, \
+    KC_LCBR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,   _______, _______, KC_RCBR  \
   ),
 
 /* Raise
@@ -95,9 +95,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------------------------------------------'
  */
   [_RAISE] = KEYMAP( \
-    KC_GRV,  KC_1     KC_2,    KC_3,    KC_4,    KC_5,                                                          _______, _______, _______, KC_MINS, KC_PLUS, KC_BSPC, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          _______, _______, _______, KC_MINS, KC_PLUS, KC_BSPC, \
     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             _______, _______, _______, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    KC_LCBR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, KC_BSLS, KC_RCBR  \
+    KC_LBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, KC_BSLS, KC_RBRC  \
   ),
 
 /* Adjust
