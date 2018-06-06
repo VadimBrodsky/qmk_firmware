ff82362179cae473106795b15d478b234a48c2a4
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-06-06 15:27:37 -0400

    Move shift to the thumb cluste

--- keyboards/orthodox/keymaps/vdk9000/keymap.c
+++ keyboards/orthodox/keymaps/vdk9000/keymap.c
@@ -59,15 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                                         ,-----------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |                                         |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------|      ,---------------------------.      |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |      |      | GUI  | Enter|Leader|      |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |      |Shift | GUI  |Leader| Shift|      |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |Lower | Alt  | Spc  | Spc  |  Ctl  | Raise|   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Ctl  |   Z  |   X  |   C  |   V  |   B  |Lower | Alt  | Spc  | Spc  |      | Raise|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------------------------------------------------------------------------------------------'
  */
   [_QWERTY] = KEYMAP( \
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_BSPC, \
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             _______,   KC_LGUI,   KC_ENT, KC_LEAD,         KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, \
-    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   LOWER,    KC_LALT,   KC_SPC,  KC_SPC, KC_LCTL,  RAISE, KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_RSPC  \
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                           KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_BSPC, \
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_LSPO,   KC_LGUI, KC_LEAD, KC_RSPC,         KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, \
+    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   LOWER,    KC_LALT,   KC_SPC,  KC_SPC,  _______,  RAISE, KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_ENT  \
   ),
 
 /* Lower
