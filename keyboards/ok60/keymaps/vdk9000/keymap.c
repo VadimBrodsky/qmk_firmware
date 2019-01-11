b574001c8771284f8eafc300cc064b4eabd83474
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-01-11 14:22:52 -0500

    Use space as fn

--- keyboards/ok60/keymaps/vdk9000/keymap.c
+++ keyboards/ok60/keymaps/vdk9000/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------------------------------------------------------------------------------------------------------------------|
      * | Shift  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   ,    |   .    |   /    | Shift  |   Fn   |
      * `--------------------------------------------------------------------------------------------------------------------'
-     *          |  Fn    |  Alt   |                            Space                             |  GUI   |   Alt  |
+     *          |  GUI   |  Alt   |                       Space / Fn                             |  GUI   |   Alt  |
      *          `--------------------------------------------------------------------------------------------------'
      */
 
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_DEL,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, MO(1),
-                 MO(1),   KC_LALT,                            KC_SPC,                             KC_RGUI, KC_RALT
+                 KC_LGUI, KC_LALT,                     LT(1, KC_SPC),                             KC_RGUI, KC_RALT
     ),
 
     /*
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------------------------------------------'
      * |  Caps  |  Vol+  |  Vol-  |  Mute  |        |        | Left   | Down   |   Up   | Right  |        |        |        |
      * |--------------------------------------------------------------------------------------------------------------------|
-     * |        |  Play  |  Next  |        |        |        |  Home  | PGDown |  PGUp  |  End   |        |        |        |
+     * |   {    |  Play  |  Next  |        |        |        |  Home  | PGDown |  PGUp  |  End   |        |        |   }    |
      * `--------------------------------------------------------------------------------------------------------------------'
      *          |        |  GUI   |                                                              |        |        |
      *          `--------------------------------------------------------------------------------------------------'
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, RESET,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI,  RGB_SAD, RGB_VAI, RGB_VAD, _______, RGB_M_P, _______, _______, _______,
         KC_CAPS, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
-        _______, KC_MPLY, KC_MNXT, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,
+     S(KC_LBRC), KC_MPLY, KC_MNXT, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, S(KC_RBRC), _______,
                  KC_LGUI, _______,                             _______,                            _______, _______
     ),
 };
